e30210510e9813e9a7dd7711d6e4de2b9f9a0db3
RogerTongTong <Roger.Tong@activenetwork.com>
2015-02-25 09:53:55 +0000

    merge the change from Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "世界，你好"
+    render text: "¡hola, mundo!"
   end
   
   def goodbye
